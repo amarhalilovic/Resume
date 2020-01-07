27f03e3c55707c014acc4f9418a029e46d8644f7
amarhalilovic <57002971+amarhalilovic@users.noreply.github.com>
2020-01-07 07:53:40 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+    Name:          Amar Halilovic
+    Course:        CS412 Web Application Development
+    Assigment:     Resume
+    Due Date:      11.1.2020.
+    Purpose:       The purpose is to make webpage for coffe shop as project for Web Application Development
+ */
+
+
+$surname = $_POST["lastname"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$name = $_POST["firstname"];
+
+
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "form";
+
+$to = 'amar.halil1303@gmail.com';
+$subject = 'New question';
+$messag = "You got message from " . $name . " " . $surname . ". His/her email is: " . $email . " The message is: " . $message;
+$headers = 'From: amar.halil1303@gmail.com';
+
+
+
+
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$q = "INSERT INTO `formq` (`firstname`, `surname`, `email`, `question`) VALUES ('$name', '$surname', '$email', '$message');";
+    if (mysqli_query($conn, $q))
+
+    mysqli_close($conn);
+    mail($to, $subject, $messag, $headers);
+
+?>
+
+<html>
+    <head>
+    <title>Submitted</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styl.css">
+    </head>
+    <body>
+   
+    <h1 id="h1">Succesfully submited.</h1>
+
+   
+    <a href="index.html"> <button id="button" class="btn btn-info"><i class="fa fa-home"></i> Home</button> </a>
+    </body>
+</html>
+
